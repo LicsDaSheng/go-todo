a5892e4841f9dd9fbad1b9695422eedb99ccbc53
Scott.Lee <once_key@126.com>
2020-06-22 18:59:50 +0800

    delete goto-web error

--- todo-web/client/client.go
+++ todo-web/client/client.go
@@ -1,11 +1,1 @@
 package client
-
-import (
-	"go.uber.org/zap"
-	"google.golang.org/grpc"
-	"oncekey/go-todo/todo-svr/service"
-)
-
-func NewGRPCClient(conn *grpc.ClientConn, log *zap.Logger) service.Service {
-
-}
--- todo-web/go.mod
+++ todo-web/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	go.uber.org/zap v1.15.0
 	google.golang.org/grpc v1.27.0
+	github.com/oncekey/go-todo v0.0.0-20200622105035-a1d90a57901c
 )
